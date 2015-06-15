103600619c89401d75873e59f72f4c531bd1e0dd
Bojan Milovanovic <Bojan Milovanovic@BojanMilovanovi>
2015-06-15 13:57:31 +0200

    prikaz menija sredjen (podeseni vertiklano meni stavke) promjenjene nepravilne sekcije iz menija

--- includes/main-nav.php
+++ includes/main-nav.php
@@ -17,36 +17,36 @@ else
     </ul>
 </li>
 <li>
-    <a href="zubna-protetika" <?php echo $typeActive == 2 ? 'class="active two-row"' : 'class="two-row"' ?>><?php echo $lang['menu']['zubna-protetika'] ?></a>
+    <a href="zubna-protetika" <?php echo $typeActive == 2 ? 'class="active "' : 'class=""' ?>><?php echo $lang['menu']['zubna-protetika'] ?></a>
     <ul>
-        <li><a href="estetska-stomatologija#section1"><?php echo $lang['menu']['cornice'] ?></a></li>
-        <li><a href="estetska-stomatologija#section2"><?php echo $lang['menu']['pres-bezmetalne'] ?></a></li>
-        <li><a href="estetska-stomatologija#section3"><?php echo $lang['menu']['cirkonijum'] ?></a></li>
-        <li><a href="estetska-stomatologija#section4"><?php echo $lang['menu']['veniri'] ?></a></li>   
-        <li><a href="estetska-stomatologija#section2"><?php echo $lang['menu']['inlej'] ?></a></li>
-        <li><a href="estetska-stomatologija#section3"><?php echo $lang['menu']['sk-proteza'] ?></a></li>
-        <li><a href="estetska-stomatologija#section4"><?php echo $lang['menu']['akrilatna'] ?></a></li>  
+        <li><a href="zubna-protetika#section1"><?php echo $lang['menu']['cornice'] ?></a></li>
+        <li><a href="zubna-protetika#section2"><?php echo $lang['menu']['pres-bezmetalne'] ?></a></li>
+        <li><a href="zubna-protetika#section3"><?php echo $lang['menu']['cirkonijum'] ?></a></li>
+        <li><a href="zubna-protetika#section4"><?php echo $lang['menu']['veniri'] ?></a></li>   
+        <li><a href="zubna-protetika#section5"><?php echo $lang['menu']['inlej'] ?></a></li>
+        <li><a href="zubna-protetika#section6"><?php echo $lang['menu']['sk-proteza'] ?></a></li>
+        <li><a href="zubna-protetika#section7"><?php echo $lang['menu']['akrilatna'] ?></a></li>  
     </ul>
 </li>
 <li>
-    <a href="implantologija" <?php echo $typeActive == 3 ? 'class="active two-row"' : 'class="two-row"' ?>><?php echo $lang['menu']['implantologija'] ?></a>
+    <a href="implantologija" <?php echo $typeActive == 3 ? 'class="active"' : 'class=""' ?>><?php echo $lang['menu']['implantologija'] ?></a>
     <ul>
         <li><a href="implantologija#section1"><?php echo $lang['menu']['straumann'] ?></a></li>   
     </ul>
 </li>
 <li>
-    <a href="oralna-hirurgija" <?php echo $typeActive == 4 ? 'class="active two-row"' : 'class="two-row"' ?>><?php echo $lang['menu']['oralna-hirurgija'] ?></a>		
+    <a href="oralna-hirurgija" <?php echo $typeActive == 4 ? 'class="active"' : 'class=""' ?>><?php echo $lang['menu']['oralna-hirurgija'] ?></a>		
      <ul>
-        <li><a href="oralna-hirurgija-implantologija#section1"><?php echo $lang['menu']['ekstrakcija-zuba'] ?></a></li>
-        <li><a href="oralna-hirurgija-implantologija#section2"><?php echo $lang['menu']['apikotomija'] ?></a></li>
-        <li><a href="oralna-hirurgija-implantologija#section3"><?php echo $lang['menu']['cistektomija'] ?></a></li>
-        <li><a href="oralna-hirurgija-implantologija#section4"><?php echo $lang['menu']['incizija'] ?></a></li>
-        <li><a href="oralna-hirurgija-implantologija#section5"><?php echo $lang['menu']['frenulektomija'] ?></a></li>
-        <li><a href="oralna-hirurgija-implantologija#section6"><?php echo $lang['menu']['nivelacija'] ?></a></li>
+        <li><a href="oralna-hirurgija#section1"><?php echo $lang['menu']['ekstrakcija-zuba'] ?></a></li>
+        <li><a href="oralna-hirurgija#section2"><?php echo $lang['menu']['apikotomija'] ?></a></li>
+        <li><a href="oralna-hirurgija#section3"><?php echo $lang['menu']['cistektomija'] ?></a></li>
+        <li><a href="oralna-hirurgija#section4"><?php echo $lang['menu']['incizija'] ?></a></li>
+        <li><a href="oralna-hirurgija#section5"><?php echo $lang['menu']['frenulektomija'] ?></a></li>
+        <li><a href="oralna-hirurgija#section6"><?php echo $lang['menu']['nivelacija'] ?></a></li>
     </ul>							
 </li>
 <li>
-    <a href="ortodoncija" <?php echo $typeActive == 5 ? 'class="active two-row"' : 'class="two-row"' ?>><?php echo $lang['menu']['ortodoncija'] ?></a>
+    <a href="ortodoncija" <?php echo $typeActive == 5 ? 'class="active "' : 'class=""' ?>><?php echo $lang['menu']['ortodoncija'] ?></a>
 </li>
 <li>
     <a href="opsta-stomatologija" <?php echo $typeActive == 6 ? 'class="active two-row"' : 'class="two-row"' ?>><?php echo $lang['menu']['opsta-stomatologija'] ?></a>
@@ -57,7 +57,7 @@ else
     </ul>
 </li>	
 <li>
-    <a href="o-nama" <?php echo $typeActive == 7 ? 'class="active"' : 'class="two-row"' ?>><?php echo $lang['menu']['o-nama'] ?></a>
+    <a href="o-nama" <?php echo $typeActive == 7 ? 'class="active"' : 'class=""' ?>><?php echo $lang['menu']['o-nama'] ?></a>
     <ul>
         <li><a href="o-nama#section1"><?php echo $lang['menu']['stom-kecman'] ?></a></li>
         <li><a href="o-nama#section2"><?php echo $lang['menu']['opste-info'] ?></a></li>
@@ -68,7 +68,7 @@ else
     </ul>
 </li>							
 <li>
-    <a href="kontakti" <?php echo $typeActive == 8 ? 'class="active"' : 'class="two-row"' ?>><?php echo $lang['menu']['kontakt'] ?></a>
+    <a href="kontakti" <?php echo $typeActive == 8 ? 'class="active"' : 'class=""' ?>><?php echo $lang['menu']['kontakt'] ?></a>
 </li>
 </ul>
 </nav>
